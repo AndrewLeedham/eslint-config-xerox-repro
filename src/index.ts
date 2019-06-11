e94c1df38943d441a501d0c2870c2835b1edcbf8
Andrew Leedham <AndrewLeedham@Outlook.com>
2019-06-11 13:01:05 +0100

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+/**
+ * Hello.
+ */
+export async function hello(): Promise<void> {
+    console.log('Hello');
+}
+
+/**
+ * World.
+ *
+ * @returns Nothing.
+ */
+export async function world(): Promise<void> {
+    console.log('World');
+}
+
+/**
+ * Test.
+ */
+export function test(): void {
+    console.log('Test');
+}
